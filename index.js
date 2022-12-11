c9d874f3ef5046c31ebf4b72b1c5203b8e111aad
robrajcs <robrajcs@gmail.com>
2022-12-11 12:19:23 +0100

    20221211.1219

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ require('./startup/validation')();
 
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+
+
+// if(process.env.NODE_ENV !== 'test') {
+//     server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+// }
 
 module.exports = server;
---
+++ tests/integration/returns.test.js
@@ -0,0 +1,67 @@
+const request = require('supertest');
+const { Rental } = require('../../models/rental');
+const { User } = require('../../models/user');
+const mongoose = require('mongoose');
+
+
+
+require('../../models/rental');
+
+describe('/api/returns', () => {
+    let server;
+    let customerId;
+    let movieId;
+    let rental;
+    
+    beforeEach( async () => { 
+        server = require('../../index'); 
+        customerId = mongoose.Types.ObjectId();
+        movieId = mongoose.Types.ObjectId();
+
+        rental = new Rental({
+            
+                customer: {
+                    _id: customerId,
+                    name: '12345', 
+                    phone: '12345'
+                  },
+                  movie: {
+                    _id: movieId,
+                    title: '12345',
+                    dailyRentalRate: 2
+                  }
+            
+        });
+        await rental.save();
+    });
+
+    afterEach( async () => {
+        server.close();
+        await Rental.remove({});
+     });
+
+     it('should return 401 if client is not logged in', async () => {
+        const res = await request(server)
+          .post('/api/returns')
+          .send({customerId: customerId, movieId: movieId});
+        expect(res.status).toBe(401);
+     });
+
+     it('should return 400 if customerId is not provided', async () => {
+        const token = new User().generateAuthToken();
+        const res = await request(server)
+          .post('/api/returns')
+          .set('x-auth-token', token)
+          .send({movieId: movieId});
+        expect(res.status).toBe(400);
+     });
+
+     it('should return 400 if movieId is not provided', async () => {
+        const token = new User().generateAuthToken();
+        const res = await request(server)
+          .post('/api/returns')
+          .set('x-auth-token', token)
+          .send({customerId: customerId});
+        expect(res.status).toBe(400);
+     });
+})
